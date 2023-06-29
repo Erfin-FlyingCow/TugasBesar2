e02004f204045248a13252b529b2c136897b2c52
Muhammad Irfan Noufal <erfintheflyngcow@gmail.com>
2023-06-29 22:02:44 +0700

    firs commit

---
+++ src/Component/Test.js
@@ -0,0 +1,9 @@
+function Test () {
+    return (
+              <h1 className="text-5xl font-bold text-center">
+                Hello world!
+              </h1>
+            );
+          }
+
+export default Test;
